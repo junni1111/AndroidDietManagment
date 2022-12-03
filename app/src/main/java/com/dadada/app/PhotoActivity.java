2f2d7570a2d83d0cae4921884c71998444af149a
Lee-Soyeon <leesoyena@gmail.com>
2022-12-04 02:12:26 +0900

    식사 기록 사진 등록 모달 완료

--- app/src/main/java/com/dadada/app/PhotoActivity.java
+++ app/src/main/java/com/dadada/app/PhotoActivity.java
@@ -1,26 +1,36 @@
 package com.dadada.app;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PhotoActivity extends AppCompatActivity {
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class PhotoActivity extends AppCompatActivity{
     ImageView photoBtn;
+    BottomSheetDialog bottomSheetDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.bottom_sheet_group_photo, null, false);
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(view);
 
         photoBtn = findViewById(R.id.photoBtn);
         photoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(PhotoActivity.this, CameraActivity.class);
-                startActivity(i);
+                bottomSheetDialog.show();
+//                Intent intent = new Intent(PhotoActivity.this, CameraActivity.class);
+//                startActivity(intent);
             }
         });
     }
