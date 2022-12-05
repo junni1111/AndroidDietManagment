51426a7199db29e6efd452dcceefbf54784e270c
junni1111 <wkszlf98@naver.com>
2022-12-06 00:18:11 +0900

    ADD : 지도 현재위치 받아오기 및 클릭 시 주소 반환

--- app/src/main/java/com/dadada/app/acitiviy/MapActivity.java
+++ app/src/main/java/com/dadada/app/acitiviy/MapActivity.java
@@ -1,11 +1,13 @@
 package com.dadada.app.acitiviy;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -23,7 +25,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -33,24 +34,43 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
         GoogleMap.OnMyLocationClickListener,
         OnMapReadyCallback {
     private final int LOCATION_ACCESS_CODE = 1234;
-    Button dietBtn;
-    private GoogleMap mMap;
+    Button dietBtn, dietBtn2;
+    private GoogleMap mMap = null;
     private Marker currentMarker = null;
+    private boolean test = false;
+    private LocationManager lm = null;
+    private LatLng selectedLocation = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-//        getWindow().setStatusBarColor(Color.TRANSPARENT);
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        LOCATION_ACCESS_CODE);
+            }
+            return;
+        }
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         assert mapFragment != null;
         mapFragment.getMapAsync(MapActivity.this);
+
+        dietBtn2 = findViewById(R.id.dietBtn2);
+        dietBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMap != null) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(getCurrentLatLng(), 15));
+                }
+            }
+        });
 
         dietBtn = findViewById(R.id.dietBtn);
         dietBtn.setOnClickListener(new View.OnClickListener() {
@@ -60,41 +80,38 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
                 startActivity(i);
             }
         });
+
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissionss
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        mMap = map;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE}, LOCATION_ACCESS_CODE);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        LOCATION_ACCESS_CODE);
             }
             return;
         }
+
         map.setMyLocationEnabled(true);
         map.setOnMyLocationButtonClickListener(this);
         map.setOnMyLocationClickListener(this);
 
-        mMap = map;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(getCurrentLatLng(), 18));
 
-        LatLng seoul = new LatLng(37.56, 126.97);
-
-        MarkerOptions options = new MarkerOptions();
-        options.position(seoul)
-                .title("서울")
-                .snippet("한국의 수도");
-        map.addMarker(options);
-
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10));
-
-
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                Toast.makeText(MapActivity.this, "" + getCurrentAddress(point),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -145,5 +162,26 @@ public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLoca
             return address.getAddressLine(0).toString();
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_ACCESS_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Intent i = new Intent(MapActivity.this, MapActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }
+    }
+
+    LatLng getCurrentLatLng() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        return new LatLng(location.getLatitude(), location.getLongitude());
     }
 }
